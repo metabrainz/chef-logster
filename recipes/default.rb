d246cb6abb0fe4916749735817e30228d3e5c73e
Oliver Charles <ollie@ocharles.org.uk>
2013-08-29 13:29:24 +0100

    logster: Add key

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@ apt_repository "kiall" do
   distribution node['lsb']['codename']
   components ["main"]
   keyserver "keyserver.ubuntu.com"
+  key "6FD8098A15B1106C229A7A029B9566D4DFA62498"
 end
 
 package "logtail"
